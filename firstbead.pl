ce0dce7bccc7c9e28e2972eb3235a1d6f8105fc3
Josh Darling <squiddarling@gmail.com>
2023-01-30 06:08:33 -0500

    added shortboard.pl comparision to JS program

--- firstbead.pl
+++ firstbead.pl
@@ -1,4 +1,3 @@
-
 #!/usr/bin/perl
 use strict;
 use warnings;
@@ -8,35 +7,36 @@ use 5.010;
 # I could do the whole thing with mostly turnary operators. 
 # The reason I didn't is I wanted to show a way that relises
 #  more on things that specific to perl. For instance 
-#  "until," hash tables, and perl short hand, etc...
-
-
-
-
+#  "until," string replacement, "my" local variables, perl short hand, etc...
 # The first time wrote this program
 # I used alt code for the black and white squres.
 # Perl does not like alt code squares.
 
 my $number = 1;
-# $intger = 0;
-my $white = "_";
-my $black = "#";
+my @the_board = (1..8);
 my @even_line = ();
 my @odd_line = ();
 
 until ($number == 9){
-my $space_color_odd = $number %2 != 0? "w": "b";
-my $space_color_even = $number %2 == 0? "w": "b";
+my $space_color_odd = $number %2 != 0? "w": "b";                #I wanted to use strint replace to add an extra step
+my $space_color_even = $number %2 == 0? "w": "b";               # but these could have just been "spaces"
 push(@odd_line,$space_color_odd);
 push(@even_line,$space_color_even);
 $number++;
 }
 
-print @odd_line;
-print "\n";
-print @even_line;
+#convert arrays to strings.
+my $odd_string = join '', @odd_line;
+my $even_string = join '',  @even_line;
 
-#chomp()
+$odd_string =~ tr/w/_/;                                         # totally unnecessary
+$odd_string =~ tr/b/#/;                                         # I just wanted to have with the string replace
+$even_string =~ tr/w/_/;                                        # operator.
+$even_string =~ tr/b/#/;
 
+for my $in (@the_board){
+    my $line_is = $in %2 != 0? $even_string : $odd_string;
+    print $line_is."\n";
+}
 
-
+#for a "fun obervation" check out the file "shortboard.pl"
---
+++ shortboard.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use 5.010;
+
+my $number = 1;
+my @the_board = (1..8);
+my @even_line = ();
+my @odd_line = ();
+
+until ($number == 9){
+my $space_color_odd = $number %2 != 0? "_": "#";
+my $space_color_even = $number %2 == 0? "_": "#";
+push(@odd_line,$space_color_odd);
+push(@even_line,$space_color_even);
+$number++;
+}
+
+my $odd_string = join '', @odd_line;
+my $even_string = join '',  @even_line;
+
+for my $in (@the_board){
+    my $line_is = $in %2 != 0? $even_string : $odd_string;
+    print $line_is."\n";
+}
+
+#A while back I wrote a program that had the same out to console in javascript. I wrote it using only turnary operators and 1 while loop.
+# I didn't write that for readablity and nested a the "short" if staments on one line like this:
+#
+# if (l %2) {lineIs = goHere ? outString : hashTag;}
+#
+# for javescript that's "compact" the total lines of that compacted javascript program is 29 lines not inculding comments
+# for bewtter readablity it really should be 35 and I didn't comment that code.
+#
+# the same program in perl also "uncommented" and using arrays... 25 lines.
