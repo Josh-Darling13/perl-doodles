b2083368a58c71aab9f1e83523522f6d9da26043
Josh Darling <squiddarling@gmail.com>
2023-01-30 05:26:54 -0500

    decided to go with easy version

--- firstbead.pl
+++ firstbead.pl
@@ -21,27 +21,22 @@ my $number = 1;
 # $intger = 0;
 my $white = "_";
 my $black = "#";
-# $line_build = "";
-my %space = ('black'=>'_','white'=>'#');
 my @even_line = ();
 my @odd_line = ();
 
 until ($number == 9){
-my $space_color_odd = $number %2 != 0? "white": "black";
-my $space_color_even = $number %2 == 0? "white": "black";
+my $space_color_odd = $number %2 != 0? "w": "b";
+my $space_color_even = $number %2 == 0? "w": "b";
 push(@odd_line,$space_color_odd);
 push(@even_line,$space_color_even);
 $number++;
 }
 
-# print @odd_line;
-# print "\n";
-# print @even_line;
+print @odd_line;
+print "\n";
+print @even_line;
 
-for my $n (@odd_line){
-    my $space_fill = $space{$n};
-    say $space_fill;
-}
+#chomp()
 
 
 
