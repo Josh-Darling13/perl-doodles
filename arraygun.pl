43252ad84b3658522fc486ae72b913bdc9f39849
Josh Darling <squiddarling@gmail.com>
2023-02-09 00:28:52 -0500

    subroutine tested and working

--- arraygun.pl
+++ arraygun.pl
@@ -8,12 +8,15 @@ my @theArray = (1,1,10,20,30,7,6,5,4,2,3,4,5,6,7,8,9,11,22,23,22,21,20,19,18,15,
 
 sub UpcountTestAd{
     my @args = @_;
-    print @args;
-    print $args[2];
-
-#    @a[$thispos]+1 == @a[$thispos+1]? true : false;
+    my $thispos = int $args[0];
+    my $posIS = $thispos + 1;
+    my $testAgainst = $thispos + 2;
+    my $primaryVal = $args[$posIS];
+    my $testLocation =  $args[$testAgainst];
+    my $isConsec = ($primaryVal+1) == $testLocation? 1 : 0;
+    return $isConsec;
 }
 
-my $thispos = 1;
+my $thispos = 13;
 
-upcountTestAd($thispos,@theArray);
+print "result ", UpcountTestAd($thispos,@theArray);
