0bfdebc0364e7125ae1948c33ade4e533914edaa
Josh Darling <squiddarling@gmail.com>
2023-02-12 22:31:41 -0500

    mucking translating a Javascript program to Perl... for fun

--- arraygun.pl
+++ arraygun.pl
@@ -1,10 +1,12 @@
 #! /usr/bin/perl
-
 use strict;
 use warnings;
 use 5.32.1;
 
 my @theArray = (1,1,10,20,30,7,6,5,4,2,3,4,5,6,7,8,9,11,22,23,22,21,20,19,18,15,12,9,6,3,555555,101,102,103,4,4,4,4,651,650,50,100,150,200,250,400,200,100);
+
+
+
 
 sub UpcountTestAd{
     my @args = @_;
@@ -20,3 +22,38 @@ sub UpcountTestAd{
 my $thispos = 13;
 
 print "result ", UpcountTestAd($thispos,@theArray);
+
+
+sub findConsecutiveOne
+    {
+    my @args = @_;
+    my $arrayLen = $#args;
+    my $consecStart = [];
+    my $i = 0;
+    my $t = 0;
+    my $c = 0;
+    my $startNum = '';
+    my $startcount = 1;
+    my $multiCount = 0;
+
+    while ($i < $arrayLen){
+        if (UpcountTestAd($i,@theArray) == 1){              # tests for 2 consecutive numbers
+            $t = $t + 1; 
+
+            if ($startcount == 1){
+                $t = $t = 1; 
+                $startcount = 0;
+            }
+
+        }
+
+
+
+        $i = $i+1;
+    }
+
+    print" \n array len: ", $arrayLen;
+
+    }
+
+findConsecutiveOne(@theArray);
